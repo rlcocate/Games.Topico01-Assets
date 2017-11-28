67295155a57c79f2bc9651fea97c8c984b7def05
logonrm <logonrm@fiap.com.br>
2017-11-27 22:40:35 -0200

    Primeiro Commit

---
+++ Assets/Scripts/Bloco.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloco : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D c){
+		print ("Houve colisão!");
+		print (c.gameObject.transform.position);
+		if (c.gameObject.tag == "Piso") Destroy (c.gameObject);
+	}
+
+	void OnCollisionStay2D(Collision2D c){
+		print ("Colidindo...");
+	}
+
+	void OnCollisionExit2D(Collision2D c){
+		print ("Parou de colidir!");
+	}
+}
